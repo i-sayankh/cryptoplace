a4f37f2bf9b17b9bf63727ae6a8951c2ffd87507
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-06-11 14:06:44 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/coin/:coinId' element={<Coin/>}/>
       </Routes>
-      
+      <Footer/>
     </div>
   )
 }
