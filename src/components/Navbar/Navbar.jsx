15ea44ca20e371ed50574fde157de956ebb22d80
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-06-13 11:59:27 +0530

    Coin Details Page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,48 +3,49 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const {setCurrency} = useContext(CoinContext);
+  const { setCurrency } = useContext(CoinContext);
 
   const currencyHandler = (event) => {
     switch (event.target.value) {
       case "inr": {
-        setCurrency({name: "inr", symbol : "₹"});
+        setCurrency({ name: "inr", symbol: "₹" });
         break;
       }
       case "usd": {
-        setCurrency({name: "usd", symbol : "$"});
+        setCurrency({ name: "usd", symbol: "$" });
         break;
       }
       case "eur": {
-        setCurrency({name: "eur", symbol : "€"});
+        setCurrency({ name: "eur", symbol: "€" });
         break;
       }
       default: {
-        setCurrency({name: "inr", symbol : "₹"});
+        setCurrency({ name: "inr", symbol: "₹" });
         break;
       }
     }
   }
-  
+
   return (
     <div className='navbar'>
-        <img src={logo} alt="" className='logo' />
-        <ul>
-            <li>Home</li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Blog</li>
-        </ul>
-        <div className="nav-right">
-            <select onChange={currencyHandler}>
-                <option value="inr">INR</option>
-                <option value="usd">USD</option>
-                <option value="eur">EUR</option>
-            </select>
-            <button>Sign Up <img src={arrow_icon} alt="" /></button>
-        </div>
+      <Link to={'/'}><img src={logo} alt="" className='logo' /></Link>
+      <ul>
+        <Link to={'/'}><li>Home</li></Link>        
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select onChange={currencyHandler}>
+          <option value="inr">INR</option>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+        </select>
+        <button>Sign Up <img src={arrow_icon} alt="" /></button>
+      </div>
     </div>
   )
 }
